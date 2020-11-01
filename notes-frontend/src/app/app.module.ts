5e4a44207512aa85bb957acfd85c403373af4e35
Younes Turki <younes.turki@EfficyLounge.local>
2020-11-01 15:23:28 +0100

    first commit

---
+++ notes-frontend/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NotesListComponent } from './pages/notes-list/notes-list.component';
+import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
+import { NoteCardComponent } from './note-card/note-card.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NotesListComponent,
+    MainLayoutComponent,
+    NoteCardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
