ff44e92428bffa0848df0efd559fc5cccdb9e8c3
Younes Turki <younes.turki@EfficyLounge.local>
2020-11-08 14:20:45 +0100

    Animation

--- notes-frontend/src/app/app.module.ts
+++ notes-frontend/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -8,6 +9,7 @@ import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
 import { NoteCardComponent } from './note-card/note-card.component';
 import { DetailsComponent } from './pages/details/details.component';
 import { FormsModule } from '@angular/forms';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,8 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- notes-frontend/src/app/pages/notes-list/notes-list.component.ts
+++ notes-frontend/src/app/pages/notes-list/notes-list.component.ts
@@ -1,3 +1,4 @@
+import { animate, query, stagger, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { Note } from 'src/app/shared/note.module';
 import { NotesService } from 'src/app/shared/notes.service';
@@ -5,7 +6,77 @@ import { NotesService } from 'src/app/shared/notes.service';
 @Component({
   selector: 'app-notes-list',
   templateUrl: './notes-list.component.html',
-  styleUrls: ['./notes-list.component.scss']
+  styleUrls: ['./notes-list.component.scss'],
+  animations: [
+    trigger('itemAnim', [
+      transition('void => *', [
+        style({
+          height: 0,
+          opacity: 0,
+          transform: 'scale(0.85)',
+          'margin-bottom': 0,
+
+          // we have to expand out the padding properties
+          paddingTop: 0,
+          paddingBottom: 0,
+          paddingRight: 0,
+          paddingLeft: 0,
+        }),
+        //we first want to animate the spacing (which includes height and marging)
+        animate('50ms', style({
+          height: '*',
+          'margin-bottom': '*',
+          paddingTop: '*',
+          paddingBottom: '*',
+          paddingRight: '*',
+          paddingLeft: '*',
+        })),
+        animate(200)
+      ]),
+
+      transition('* => void', [
+        // first scale up
+        animate(50, style({
+          transform: 'scale(1.05)'
+        })),
+        // then scale down while beginning the fade out
+        animate(50, style({
+          transform: 'scale(1)',
+          opacity: 0.75
+        })),
+        // scale down and fade out
+        animate('120ms ease-out', style({
+          transform: 'scale(0.68)',
+          opacity: 0
+        })),
+        // then animate the spacing (which include light and margin)
+        animate('150ms ease-out',style({
+          height: 0,
+          paddingTop: 0,
+          paddingBottom: 0,
+          paddingRight: 0,
+          paddingLeft: 0,
+          'margin-bottom': '0',
+        }))
+      ])
+    ]),
+
+    trigger('listAnim',[
+      transition('* => *', [
+        query(':enter', [
+          style({
+            opacity: 0,
+            height: 0
+          }),
+          stagger(100, [
+            animate('0.2s ease')
+          ])
+        ], {
+          optional: true
+        })
+      ])
+    ])
+  ]
 })
 export class NotesListComponent implements OnInit {
 
