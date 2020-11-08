4177757e0c7a9c573f7bb4f4a8e3d54a72f236a1
Younes Turki <younes.turki@EfficyLounge.local>
2020-11-08 15:28:37 +0100

    sorting notes

--- notes-frontend/src/app/pages/notes-list/notes-list.component.ts
+++ notes-frontend/src/app/pages/notes-list/notes-list.component.ts
@@ -1,5 +1,5 @@
 import { animate, query, stagger, style, transition, trigger } from '@angular/animations';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Note } from 'src/app/shared/note.module';
 import { NotesService } from 'src/app/shared/notes.service';
 
@@ -82,16 +82,25 @@ export class NotesListComponent implements OnInit {
 
   notes: Note[] = new Array<Note>();
   filteredNotes: Note[] = new Array<Note>();
+  @ViewChild('filterInput',{static: true})filterInputElRef : ElementRef<HTMLInputElement>;
 
   constructor(private notesService: NotesService) { }
 
   ngOnInit(): void {
    this.notes =  this.notesService.getAll();
-   this.filteredNotes = this.notes;
+   //this.filteredNotes = this.notesService.getAll();
+   this.filter('');
   }
 
-  deleteNote(id: number){
-    this.notesService.delete(id);
+  deleteNote(note: Note){
+    let noteId = this.notesService.getId(note);
+    this.notesService.delete(noteId);
+    this.filter(this.filterInputElRef.nativeElement.value);
+    }
+
+    getUrl(note: Note){
+      let noteId = this.notesService.getId(note);
+      return noteId;
     }
 
     filter(query: string){
@@ -117,6 +126,9 @@ export class NotesListComponent implements OnInit {
       // so we first must remove the duplicates
       let uniqueResults = this.removeDuplicates(allResults);
       this.filteredNotes = uniqueResults;
+
+      // sort by relevancy
+      this.sortByRelevancy(allResults);
     }
 
     removeDuplicates(arr: Array<any>): Array<any> {
@@ -139,5 +151,32 @@ export class NotesListComponent implements OnInit {
       })
 
       return relevantNotes;
+    }
+
+    sortByRelevancy(searchResults: Note[]){
+      // This metod will calculate the relevancy of a note based on the number of times it appeats in
+      // the search results
+
+      let noteCountObj: Object = {}; // format - key:value => NoteId:number (note object id: count)
+
+      searchResults.forEach(note => {
+        let noteId = this.notesService.getId(note); // get the notes id
+
+        if(noteCountObj[noteId]) {
+          noteCountObj[noteId] += 1;
+        } else {
+          noteCountObj[noteId] = 1;
+        }
+      })
+
+      this.filteredNotes = this.filteredNotes.sort((a: Note, b: Note) => {
+        let aId = this.notesService.getId(a);
+        let bId = this.notesService.getId(b);
+
+        let aCount = noteCountObj[aId];
+        let bCount = noteCountObj[bId];
+
+        return bCount - aCount;
+      })
     }
 }
